00c702b5503b306ca0c4ab3c05893bdd3c9a4eb6
Stephen Lazaro <stephen.lazaro@gmail.com>
2015-07-22 16:04:09 -0700

    Filled in cachematrix.R with code.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creates an ADT of matrix with cacheing of inverse
+## Takes a matrix, returns a list object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) i <<- inverse 
+  getInverse <- function() i
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function checks if inverse has been cached,
+## If not, it calculates it and stores it
+## Else, simply returns stored. Must take special matrix object as supra
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getInverse()
+        if(!is.null(i)) {
+          message("getting cached data")
+          return(i)
+        }
+        data <- x$get()
+        i <- solve(data)
+        x$setInverse(i)
+        i
 }
